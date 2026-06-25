f3eccc3fc0af8b16872e6bf5272b24947400e0a3
trojash <tonyestuardor@outlook.com>
2026-06-25 13:49:12 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const input1 = document.getElementById("textBox1");
+const celsius1 = document.getElementById("fahBtn");
+const fahrenheit1 = document.getElementById("celsiusBtn");
+const submitBtn = document.getElementById("mySubmit");
+const results = document.getElementById("results");
+
+function conversionFunction (event){
+    event.preventDefault();
+
+    let inputCalculation = Number(input1.value); //get the user input number
+    if(inputCalculation == "" || isNaN(inputCalculation)){
+        alert("Enter a correct field");
+    }
+    else if (celsius1.checked) { 
+        let fahResult = (inputCalculation *(9/5)) + 32;
+        results.textContent = `${inputCalculation}°C is ${fahResult.toFixed(1)}°F`; 
+    }else if(fahrenheit1.checked){
+        let celsius = (inputCalculation -32) * 5/9;
+        results.textContent = `${inputCalculation}°F is ${celsius.toFixed(1)}°C`;
+    }else{
+        inputCalculation = "";
+    }
+
+    
+}
+submitBtn.addEventListener("click", conversionFunction);
+
+function resetCalculator(){
+    
+}
